fda502a42e724ebd11046ce0218e7d5d6122a17a
Lucas Anderson <lucasanderson1999@gmail.com>
2023-01-17 01:28:19 -0500

    working code

---
+++ AbsFinder.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace AnyRadiance {
+    internal class AbsFinder : MonoBehaviour {
+        private GameObject _abs;
+        private bool _assigned;
+
+        private void Start() {
+            AnyRadiance.instance.Log("Added AbsFinder MonoBehaviour");
+        }
+
+        private void Update() {
+            if (_abs == null)
+            {
+                _assigned = false;
+                _abs = GameObject.Find("Absolute Radiance");
+            }
+
+            if (_assigned || _abs == null)
+            {
+                return;
+            }
+
+            _assigned = true;
+            AnyRadiance.instance.Log("Found the Radiance!");
+            _abs.AddComponent<Abs>();
+        }
+    }
+}
